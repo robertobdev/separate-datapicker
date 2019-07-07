1001c60448793bda28eb24598bff824bfbc3fb37
Roberto Barbosa <robertojf95@gmail.com>
2019-07-07 20:37:32 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'separate-datepicker';
+
+  months: Array<{ value: number, name: string }> = [
+    { value: 1, name: 'Jan' },
+    { value: 2, name: 'Feb' },
+    { value: 3, name: 'Mar' },
+    { value: 4, name: 'Abr' },
+    { value: 5, name: 'Mai' },
+    { value: 6, name: 'Jun' },
+    { value: 7, name: 'Jul' },
+    { value: 8, name: 'Ago' },
+    { value: 9, name: 'Set' },
+    { value: 10, name: 'Out' },
+    { value: 11, name: 'Nov' },
+    { value: 12, name: 'Dec' }
+  ];
+  years: Array<number> = [];
+
+  days: Array<number> = [];
+
+  answer(value) {
+    console.log(value);
+  }
+  constructor() {
+    let year: any = new Date();
+    year = year.getFullYear();
+    console.log(year);
+    for (let index = year; index > year - 90; index--) {
+      this.years.push(index);
+    }
+    for (let index = 1; index < 32; index++) {
+      this.days.push(index);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatButtonModule, MatMenuModule, MatInputModule, MatIconModule} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatMenuModule, MatInputModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
